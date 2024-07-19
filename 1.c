9fe33e7c85dd345d772a96b78d11e1753ec9fbf0
ChaHyeongChang <118725822+ChaHyeongChang@users.noreply.github.com>
2024-07-19 21:17:32 +0900

    first test
    깃허브 처음 올리는거

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main(void){
+
+
+
+    char a[10];
+    char b[10];
+
+    scanf("%s",a);
+    scanf("%s",b);
+    
+    printf("%s\n",a);
+    printf("%s\n",a);
+
+
+    return 0;
+}
