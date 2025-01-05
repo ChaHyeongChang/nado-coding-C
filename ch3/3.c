4f78283d464f89a8f987b3109b2d2de9e9e9fbd4
ChaHyeongChang <118725822+ChaHyeongChang@users.noreply.github.com>
2025-01-06 04:30:45 +0900

    Update 3.c

--- ch3/3.c
+++ ch3/3.c
@@ -1,1 +1,29 @@
 
+#include <stdio.h>
+
+int main(void){
+    
+    for(int i = 0; i <= 5; i++){
+        for(int j = 1; j <= 5 - i; j++){
+            printf(" ");
+        }
+
+        for(int k = 1; k <= 2 * i + 1; k++){
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+
+    for(int a = 1; a <= 5; a++){
+        for(int b = 1; b <= a; b++){
+            printf(" ");
+        }
+        for(int c = 1; c < 12 - (2 * a) ; c++){
+            printf("*");
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
