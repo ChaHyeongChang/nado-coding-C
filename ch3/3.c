f38c6153ea50fdde27da6c7edd71da6295384c19
ChaHyeongChang <118725822+ChaHyeongChang@users.noreply.github.com>
2025-01-06 19:13:22 +0900

    Update 3.c

--- ch3/3.c
+++ ch3/3.c
@@ -25,5 +25,41 @@ int main(void){
         printf("\n");
 
     }
+
+    
+   /* for (int i = 0; i < 5; i++){
+        for(int j = 0; j < 5 - i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    */
+
+    /*
+    for(int i = 5; i > 0; i--){
+        for(int j = i; j > 0; j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    */
+
+    ////////피라미드 쌓기////////
+    /*
+            int floor;
+    printf("몇 층으로 쌓겟습니까?(5~100) : ");
+    scanf("%d", &floor);
+
+    for(int i = 0; i < floor; i++){
+        for(int j = 0; j < floor - i - 1; j++){
+            printf(" ");
+        }
+        for(int k = 0; k < 2 * i + 1; k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    */
+   
     return 0;
 }
