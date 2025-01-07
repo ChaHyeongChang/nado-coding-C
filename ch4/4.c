a65b87a0b6eaaa7c392c218113fcb5f971b081b8
ChaHyeongChang <118725822+ChaHyeongChang@users.noreply.github.com>
2025-01-08 00:06:03 +0900

    Update 4.c

--- ch4/4.c
+++ ch4/4.c
@@ -32,6 +32,39 @@ int main(void){
             break;
         }
     }
+
+    //////while문 이용(무한루프 활용)////
+    /*
+        int num = 4;
+    printf("정답 : %d\n", num);
+    int answer = 0;
+    int chance = 5;
+    
+    while (1){
+        printf("남은 기회 %d번\n", chance--);
+        printf("숫자를 맞혀 보세요.(1~100) : ");
+        scanf("%d", &answer);
+
+        if (answer > num){
+            printf("Down%n");
+        }
+        else if (answer < num){
+            printf("UP\n");
+        }
+        else if (answer == num){
+            printf("정답입니다\n");
+            break;
+        }
+        else{
+            printf("알 수 없는 오류가 발생\n");
+        }
+
+        if (chance == 0){
+            printf("모든 기회를 사용했어요. 실패!\n");
+            break;
+        }
+    }
+    */
     
     return 0;
 }
